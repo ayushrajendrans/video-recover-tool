ef8d95597bc525420cd790847165ccd2c257bb82
ayushraj <seenarajendran007@gmail.com>
2026-02-28 16:22:19 +0530

    Fix download button to point to actual setup exe

--- website/src/components/Hero.tsx
+++ website/src/components/Hero.tsx
@@ -21,7 +21,7 @@ export default function Hero() {
                     </p>
 
                     <div className="hero__actions">
-                        <a href="#download" className="btn btn-primary hero__btn">
+                        <a href="/Pro-Video-Repair-Setup.exe" download className="btn btn-primary hero__btn">
                             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
                                 <polyline points="7 10 12 15 17 10" />
